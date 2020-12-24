631280ecace52d45248f06d5070a18a7be7330e1
JacksonDagger <JacksonDDagger@gmail.com>
2020-12-24 12:44:07 -0500

    knexfile added

---
+++ backend/express/knexfile.js
@@ -0,0 +1,46 @@
+module.exports = {
+    test: {
+      client: "sqlite3",
+      useNullAsDefault: true,
+      connection: {
+        filename: "dev.sqlite3",
+        database: "dev_oneroof",
+        migrations: {
+            directory: __dirname + "/migrations"
+        },
+        seeds: {
+            directory: __dirname + "/seeds"
+        },
+      }
+    },
+
+    production: {
+        client: "sqlite3",
+        useNullAsDefault: true,
+        connection: {
+          filename: "dev.sqlite3",
+          database: "dev_oneroof",
+          migrations: {
+              directory: __dirname + "/migrations"
+          },
+          seeds: {
+              directory: __dirname + "/seeds"
+          },
+        }
+      },
+
+      development: {
+        client: "sqlite3",
+        useNullAsDefault: true,
+        connection: {
+          filename: "dev.sqlite3",
+          database: "dev_oneroof",
+          migrations: {
+              directory: __dirname + "/migrations"
+          },
+          seeds: {
+              directory: __dirname + "/seeds"
+          },
+        }
+      }
+  };
