ba9d8a32f2504b5a7b7c2924480f9efe207afa7d
JacksonDagger <JacksonDDagger@gmail.com>
2020-12-24 23:15:25 -0500

    fix camelcase

--- backend/express/modules/roommates.js
+++ backend/express/modules/roommates.js
@@ -73,13 +73,13 @@ class Roommates {
                return {id: roommate.roommate_id, name: roommate.roommate_name};
              }
                 
-            var housesList = await this.knex("houses")
+            var houses_list = await this.knex("houses")
                 .where("house_id", roommate.roommate_house)
                 .select("house_admin");
             
 
             console.log(houses_list);
-            var house = housesList[0];
+            var house = houses_list[0];
             
             console.log(house);
 
